26a261230ebbd9bb12e40058361887ab6eb57214
M VAISHNAVI <M VAISHNAVI@DESKTOP-KQ1FHP8>
2025-11-19 10:55:40 +0530

    SPRING

---
+++ springbasics/src/main/java/com/tnsif/springbasics/SpringbasicsApplication.java
@@ -0,0 +1,14 @@
+package com.tnsif.springbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbasicsApplication.class, args);
+		System.out.println("Heloooo Dearrrr Coderrrrssss");
+	}
+
+}
