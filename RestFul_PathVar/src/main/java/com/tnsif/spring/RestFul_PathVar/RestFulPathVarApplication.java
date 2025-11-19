0307ce4ae4e27faee3873a185dfb80e019fc028c
M VAISHNAVI <M VAISHNAVI@DESKTOP-KQ1FHP8>
2025-11-19 10:52:04 +0530

    SPRING

---
+++ RestFul_PathVar/src/main/java/com/tnsif/spring/RestFul_PathVar/RestFulPathVarApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.spring.RestFul_PathVar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulPathVarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulPathVarApplication.class, args);
+	}
+
+}
---
+++ RestFul_PathVar/src/test/java/com/tnsif/spring/RestFul_PathVar/RestFulPathVarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.spring.RestFul_PathVar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulPathVarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
